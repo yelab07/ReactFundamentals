0b0687b6c074e281e58473902b90c31758bb7a6d
yelab07 <79489573+yelab07@users.noreply.github.com>
2022-07-06 23:55:36 -0500

    updates

---
+++ contacts/src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+const contacts = [
+  {
+    id: "karen",
+    name: "Karen Isgrigg",
+    handle: "karen_isgrigg",
+    avatarURL: "http://localhost:5001/karen.jpg",
+  },
+  {
+    id: "richard",
+    name: "Richard Kalehoff",
+    handle: "richardkalehoff",
+    avatarURL: "http://localhost:5001/richard.jpg",
+  },
+  {
+    id: "tyler",
+    name: "Tyler McGinnis",
+    handle: "tylermcginnis",
+    avatarURL: "http://localhost:5001/tyler.jpg",
+  },
+];
+const ContactList = () => {
+  const contacts = [{ name: "abebe" }, { name: "kebede" }, { name: "ayalew" }];
+  return (
+    <ol>
+      {contacts.map((item) => (
+        <li key={item.name}>{item.name}</li>
+      ))}
+    </ol>
+  );
+};
+function App() {
+  return (
+    <div className="App">
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
